ecbd305702d05ded5f6ab4d33cf9fe82366007db
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-03-06 00:03:54 +0100

    Update Ejercicio1.py

--- Ej1/Ejercicio1.py
+++ Ej1/Ejercicio1.py
@@ -1,3 +1,14 @@
 class Alumno:
-    def___init__(self, cadena):
-        self.nombre = cadena
+    def ___init__(self, cadena):
+        self.cadena = cadena
+    
+    @staticmethod
+    def voltear(cadena):
+        cadena_ordenada = cadena[::-1]
+        nota = cadena_ordenada[0:2]
+        nombre = cadena_ordenada[2:]
+        return nombre + "ha sacado un" + nota
+
+
+cadena = "zeréP nauJ,01"
+Alumno(cadena)
